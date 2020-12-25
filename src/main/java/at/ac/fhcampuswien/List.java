9e7a67e0d2d4eec796144cbfd47abefd9f27699f
Lukas <lukas.jaiczay@fh-campuswien.ac.at>
2020-12-25 11:16:25 +0100

    new class List and Method

---
+++ src/main/java/at/ac/fhcampuswien/List.java
@@ -0,0 +1,39 @@
+package at.ac.fhcampuswien;
+
+import java.util.ArrayList;
+
+public class List {
+
+   public static java.util.List<String> EasyMode= new ArrayList<String>();
+   public static java.util.List<String> ChallengingMode= new ArrayList<String>();
+
+    public static void SelectEasyMode() {
+        EasyMode.add("twenty");
+        EasyMode.add("orange");
+        EasyMode.add("market");
+        EasyMode.add("coffee");
+        EasyMode.add("nature");
+        EasyMode.add("people");
+        EasyMode.add("mirror");
+        EasyMode.add("family");
+        EasyMode.add("office");
+        EasyMode.add("silver");
+        EasyMode.add("parent");
+        EasyMode.add("period");
+    }
+    public static void SelectModeChallenging(){
+        ChallengingMode.add("strawberry");
+        ChallengingMode.add("basketball");
+        ChallengingMode.add("perfection");
+        ChallengingMode.add("technology");
+        ChallengingMode.add("watermelon");
+        ChallengingMode.add("government");
+        ChallengingMode.add("friendship");
+        ChallengingMode.add("university");
+        ChallengingMode.add("helicopter");
+        ChallengingMode.add("generation");
+        ChallengingMode.add("apocalypse");
+        ChallengingMode.add("restaurant");
+        ChallengingMode.add("depression");
+    }
+}
--- src/main/java/at/ac/fhcampuswien/Startseite.java
+++ src/main/java/at/ac/fhcampuswien/Startseite.java
@@ -13,7 +13,6 @@ import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class Startseite implements Initializable {
@@ -22,8 +21,7 @@ public class Startseite implements Initializable {
     public TextField player;
     public Button startButton;
     public ComboBox mode;
-    List<String> EasyMode= new ArrayList<String>();
-    List<String> ChallengingMode= new ArrayList<String>();
+
 
 
     @Override
@@ -47,34 +45,10 @@ public class Startseite implements Initializable {
     }
 
     public void SelectMode() {
-        EasyMode.add("twenty");
-        EasyMode.add("orange");
-        EasyMode.add("market");
-        EasyMode.add("coffee");
-        EasyMode.add("nature");
-        EasyMode.add("people");
-        EasyMode.add("mirror");
-        EasyMode.add("family");
-        EasyMode.add("office");
-        EasyMode.add("silver");
-        EasyMode.add("parent");
-        EasyMode.add("period");
+        List.SelectEasyMode();
+        List.SelectModeChallenging();
     }
-    public void SelectModeChallenging(){
-        ChallengingMode.add("strawberry");
-        ChallengingMode.add("basketball");
-        ChallengingMode.add("perfection");
-        ChallengingMode.add("technology");
-        ChallengingMode.add("watermelon");
-        ChallengingMode.add("government");
-        ChallengingMode.add("friendship");
-        ChallengingMode.add("university");
-        ChallengingMode.add("helicopter");
-        ChallengingMode.add("generation");
-        ChallengingMode.add("apocalypse");
-        ChallengingMode.add("restaurant");
-        ChallengingMode.add("depression");
-    }
+
 
 
 }
