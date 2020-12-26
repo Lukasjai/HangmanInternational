62e987e2a0652c716581f8582c90b0fd7d6be256
Serhii <sergey.dn2003@gmail.com>
2020-12-26 23:18:39 +0200

    Lists updated

--- src/main/java/at/ac/fhcampuswien/Method.java
+++ src/main/java/at/ac/fhcampuswien/Method.java
@@ -4,9 +4,10 @@ public class Method {
     static int r;
 
     public static int getRandom() {
-        r = (int) (Math.random() * 10);
+        r = (int) (Math.random() * 50);
         return r;
     }
+
 
 
 }
