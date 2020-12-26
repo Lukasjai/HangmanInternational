4f771c46fb3a013d85982b7f59725903bb1fe0d1
amina <amina.hussein@stud.fh-campuswien.ac.at>
2020-12-26 20:41:36 +0100

    extra popups

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     public static Stage window;
 
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -28,12 +29,24 @@ public class App extends Application {
         });
     }
 
+
+
     private void closeProgram() {
         Boolean answer = ConfirmBox.display("Close Window", "Sure you want to exit?");
         if (answer)
+            closeProgram2();
+    }
+
+    private void closeProgram2() {
+        Boolean answer = ConfirmBox2.display("Close Window", "Are you really sure?");
+        if (answer)
+            closeProgram3();
+    }
+
+    private void closeProgram3() {
+        Boolean answer = ConfirmBox3.display("Close Window", "Are you 100% sure?");
+        if (answer)
             window.close();
-
-
     }
 
     public static void closeWindow() {
--- src/main/java/at/ac/fhcampuswien/Playwindow.java
+++ src/main/java/at/ac/fhcampuswien/Playwindow.java
@@ -167,7 +167,19 @@ public class Playwindow extends Startseite implements Initializable {
     }
 
     private void closeProgram() {
-        boolean answer = ConfirmBox.display("Close Window", "Sure you want to exit?");
+        Boolean answer = ConfirmBox.display("Close Window", "Sure you want to exit?");
+        if (answer)
+            closeProgram2();
+    }
+
+    private void closeProgram2() {
+        Boolean answer = ConfirmBox2.display("Close Window", "Are you really sure?");
+        if (answer)
+            closeProgram3();
+    }
+
+    private void closeProgram3() {
+        Boolean answer = ConfirmBox3.display("Close Window", "Are you 100% sure?");
         if (answer)
             window.close();
     }
