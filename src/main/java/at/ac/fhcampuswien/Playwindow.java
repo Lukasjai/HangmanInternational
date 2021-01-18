fca7496d9ba2724293b3c06e768cc6e163bd09f3
Lukas <lukas.jaiczay@fh-campuswien.ac.at>
2021-01-18 13:55:53 +0100

    Legs and head appear in a smooth way :) with the help of Prof Strommer we initialised the Fade Transition

--- src/main/java/at/ac/fhcampuswien/Playwindow.java
+++ src/main/java/at/ac/fhcampuswien/Playwindow.java
@@ -1,8 +1,13 @@
 package at.ac.fhcampuswien;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,11 +16,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 
 public class Playwindow extends Homepage implements Initializable {
 
@@ -72,7 +79,7 @@ public class Playwindow extends Homepage implements Initializable {
     // Here we use two Arrays one to compare the letter of the pressed Button
     // and the other is used to print out at the window and add the new letters if they are right
     // if the letter is wrong the lifecounter is decreased
-    public void replaceButtons(Button button) throws IOException {
+    public void replaceButtons(Button button) throws IOException, InterruptedException {
         for (int i = 0; i < actual.length; i++) {
             if (actual[i].equals(button.getText() + " ")) {
                 compare[i] = button.getText() + " ";
@@ -87,6 +94,7 @@ public class Playwindow extends Homepage implements Initializable {
             lifescounter--;
         }
         if (actualWord.equals(compareWord)) {
+            TimeUnit.SECONDS.sleep(2);
             winWindow();
             lifescounter = 6;
             close = 10;
@@ -97,29 +105,76 @@ public class Playwindow extends Homepage implements Initializable {
     }
 
     //Function that makes visible our Hangman by decreased lifes
-    public int lifeCounter() throws IOException {
+    public int lifeCounter() throws IOException, InterruptedException {
         lifes.setText(String.valueOf(lifescounter));
         mistakes = 0;
         switch (lifescounter) {
             case 5:
+                FadeTransition ft1 = new FadeTransition(Duration.millis(1000), hangman1);
+                ft1.setFromValue(0.1);
+                ft1.setToValue(1.0);
                 hangman1.setVisible(true);
+                ft1.play();
                 break;
             case 4:
+                FadeTransition ft2 = new FadeTransition(Duration.millis(1000), hangman2);
+                ft2.setFromValue(0.1);
+                ft2.setToValue(1.0);
                 hangman2.setVisible(true);
+                ft2.play();
                 break;
             case 3:
+                FadeTransition ft3 = new FadeTransition(Duration.millis(1000), hangman3);
+                ft3.setFromValue(0.1);
+                ft3.setToValue(1.0);
                 hangman3.setVisible(true);
+                ft3.play();
                 break;
             case 2:
+                FadeTransition ft4 = new FadeTransition(Duration.millis(1000), hangman4);
+                ft4.setFromValue(0.1);
+                ft4.setToValue(1.0);
                 hangman4.setVisible(true);
+                ft4.play();
                 break;
             case 1:
+                FadeTransition ft5 = new FadeTransition(Duration.millis(1000), hangman5);
+                ft5.setFromValue(0.1);
+                ft5.setToValue(1.0);
                 hangman5.setVisible(true);
+                ft5.play();
                 break;
             case 0:
+
+                FadeTransition ft6 = new FadeTransition(Duration.millis(1000), hangman6);
+                ft6.setFromValue(0.1);
+                ft6.setToValue(1.0);
                 hangman6.setVisible(true);
+                ft6.play();
+
+                FadeTransition ft7 = new FadeTransition(Duration.millis(1000), hangman7);
+                ft7.setFromValue(0.1);
+                ft7.setToValue(1.0);
                 hangman7.setVisible(true);
-                looseWindow();
+                ft7.play();
+
+                //Made by Mr.Strommer to solve the Problem
+                ft6.setOnFinished(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        try {
+                            looseWindow();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+
+                //TimeUnit.SECONDS.sleep(5);
+
+
                 lifescounter = 6;
                 close = 20;
                 startunderlines = "";
@@ -179,132 +234,132 @@ public class Playwindow extends Homepage implements Initializable {
 
 
     //when the button is pressed the replaceButton function is opened and the button gets invisible
-    public void hideButtonA() throws IOException {
+    public void hideButtonA() throws IOException, InterruptedException {
         replaceButtons(buttonA);
         buttonA.setVisible(false);
     }
 
-    public void hideButtonB() throws IOException {
+    public void hideButtonB() throws IOException, InterruptedException {
         replaceButtons(buttonB);
         buttonB.setVisible(false);
     }
 
-    public void hideButtonC() throws IOException {
+    public void hideButtonC() throws IOException, InterruptedException {
         replaceButtons(buttonC);
         buttonC.setVisible(false);
     }
 
-    public void hideButtonD() throws IOException {
+    public void hideButtonD() throws IOException, InterruptedException {
         replaceButtons(buttonD);
         buttonD.setVisible(false);
     }
 
-    public void hideButtonE() throws IOException {
+    public void hideButtonE() throws IOException, InterruptedException {
         replaceButtons(buttonE);
         buttonE.setVisible(false);
     }
 
-    public void hideButtonF() throws IOException {
+    public void hideButtonF() throws IOException, InterruptedException {
         replaceButtons(buttonF);
         buttonF.setVisible(false);
     }
 
-    public void hideButtonG() throws IOException {
+    public void hideButtonG() throws IOException, InterruptedException {
         replaceButtons(buttonG);
         buttonG.setVisible(false);
     }
 
-    public void hideButtonH() throws IOException {
+    public void hideButtonH() throws IOException, InterruptedException {
         replaceButtons(buttonH);
         buttonH.setVisible(false);
     }
 
-    public void hideButtonI() throws IOException {
+    public void hideButtonI() throws IOException, InterruptedException {
         replaceButtons(buttonI);
         buttonI.setVisible(false);
     }
 
-    public void hideButtonJ() throws IOException {
+    public void hideButtonJ() throws IOException, InterruptedException {
         replaceButtons(buttonJ);
         buttonJ.setVisible(false);
     }
 
-    public void hideButtonK() throws IOException {
+    public void hideButtonK() throws IOException, InterruptedException {
         replaceButtons(buttonK);
         buttonK.setVisible(false);
     }
 
-    public void hideButtonL() throws IOException {
+    public void hideButtonL() throws IOException, InterruptedException {
         replaceButtons(buttonL);
         buttonL.setVisible(false);
     }
 
-    public void hideButtonM() throws IOException {
+    public void hideButtonM() throws IOException, InterruptedException {
         replaceButtons(buttonM);
         buttonM.setVisible(false);
     }
 
-    public void hideButtonN() throws IOException {
+    public void hideButtonN() throws IOException, InterruptedException {
         replaceButtons(buttonN);
         buttonN.setVisible(false);
     }
 
-    public void hideButtonO() throws IOException {
+    public void hideButtonO() throws IOException, InterruptedException {
         replaceButtons(buttonO);
         buttonO.setVisible(false);
     }
 
-    public void hideButtonP() throws IOException {
+    public void hideButtonP() throws IOException, InterruptedException {
         replaceButtons(buttonP);
         buttonP.setVisible(false);
     }
 
-    public void hideButtonQ() throws IOException {
+    public void hideButtonQ() throws IOException, InterruptedException {
         replaceButtons(buttonQ);
         buttonQ.setVisible(false);
     }
 
-    public void hideButtonR() throws IOException {
+    public void hideButtonR() throws IOException, InterruptedException {
         replaceButtons(buttonR);
         buttonR.setVisible(false);
     }
 
-    public void hideButtonS() throws IOException {
+    public void hideButtonS() throws IOException, InterruptedException {
         replaceButtons(buttonS);
         buttonS.setVisible(false);
     }
 
-    public void hideButtonT() throws IOException {
+    public void hideButtonT() throws IOException, InterruptedException {
         replaceButtons(buttonT);
         buttonT.setVisible(false);
     }
 
-    public void hideButtonU() throws IOException {
+    public void hideButtonU() throws IOException, InterruptedException {
         replaceButtons(buttonU);
         buttonU.setVisible(false);
     }
 
-    public void hideButtonV() throws IOException {
+    public void hideButtonV() throws IOException, InterruptedException {
         replaceButtons(buttonV);
         buttonV.setVisible(false);
     }
 
-    public void hideButtonW() throws IOException {
+    public void hideButtonW() throws IOException, InterruptedException {
         replaceButtons(buttonW);
         buttonW.setVisible(false);
     }
 
-    public void hideButtonX() throws IOException {
+    public void hideButtonX() throws IOException, InterruptedException {
         replaceButtons(buttonX);
         buttonX.setVisible(false);
     }
 
-    public void hideButtonY() throws IOException {
+    public void hideButtonY() throws IOException, InterruptedException {
         replaceButtons(buttonY);
         buttonY.setVisible(false);
     }
 
-    public void hideButtonZ() throws IOException {
+    public void hideButtonZ() throws IOException, InterruptedException {
         replaceButtons(buttonZ);
         buttonZ.setVisible(false);
     }
